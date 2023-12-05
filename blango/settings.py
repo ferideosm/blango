7e55185152a6ff24b8e73d933df223e41fe854ed
Feride Osmanova <ferideosm@gmail.com>
2023-12-05 15:41:37 +0000

    Finish database optimization

--- blango/settings.py
+++ blango/settings.py
@@ -60,9 +60,12 @@ class Dev(Configuration):
         'django.contrib.staticfiles',
         'blog',
         'crispy_forms',
-        'crispy_bootstrap5'
+        'crispy_bootstrap5',
+        "debug_toolbar",
     ]
     
+    INTERNAL_IPS = ["192.168.11.179"]
+
     LOGGING = {
         "version": 1,
         "disable_existing_loggers": False,
@@ -85,6 +88,7 @@ class Dev(Configuration):
         },
     }
     MIDDLEWARE = [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
         'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
